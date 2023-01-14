f58b2b19ab7444b945a25afbb8f3c389d4c8825b
Nagendrapanga <nagendrapanga1069@gmail.com>
2023-01-14 18:38:27 +0530

    blog

--- src/main/java/com/driver/services/BlogService.java
+++ src/main/java/com/driver/services/BlogService.java
@@ -19,20 +19,28 @@ public class BlogService {
     BlogRepository blogRepository1;
 
     @Autowired
-    ImageService imageService1;
+    ImageService imageRepository;
 
     @Autowired
     UserRepository userRepository1;
 
     public List<Blog> showBlogs(){
         //find all blogs
+        return blogRepository1.findAll();
 
     }
 
     public void createAndReturnBlog(Integer userId, String title, String content) {
         //create a blog at the current time
+        Blog blog = new Blog(title, content);
+        User user = userRepository1.findById(userId).get();
+        List<Blog> listofblogs = user.getBlogList();
+        listofblogs.add(blog);
+        user.setBlogList(listofblogs);
 
         //updating the blog details
+        blogRepository1.save(blog);
+        userRepository1.save(user);
 
         //Updating the userInformation and changing its blogs
 
@@ -40,13 +48,28 @@ public class BlogService {
 
     public Blog findBlogById(int blogId){
         //find a blog
+        return blogRepository1.findById(blogId).get();
     }
 
     public void addImage(Integer blogId, String description, String dimensions){
         //add an image to the blog after creating it
+        Image image = new Image();
+        image.setDescription(description);
+        image.setDimensions(dimensions);
+
+        Blog blog = blogRepository1.findById(blogId).get();
+        List<Image> imageList = blog.getImageList();
+        imageList.add(image);
+        image.setBlog(blog);
+        blog.setImageList(imageList);
+       blogRepository1.save(blog);
+        imageRepository.save(image);
+
     }
 
     public void deleteBlog(int blogId){
         //delete blog and corresponding images
+        blogRepository1.deleteById(blogId);
+
     }
 }
--- src/main/java/com/driver/services/ImageService.java
+++ src/main/java/com/driver/services/ImageService.java
@@ -2,29 +2,56 @@ package com.driver.services;
 
 import com.driver.models.*;
 import com.driver.repositories.ImageRepository;
+import com.driver.repositories.BlogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 @Service
 public class ImageService {
     @Autowired
     ImageRepository imageRepository2;
+    @Autowired
+    BlogRepository blogRepository;
 
     public Image createAndReturn(Blog blog, String description, String dimensions){
         //create an image based on given parameters and add it to the imageList of given blog
+        Image image = new Image(description,dimensions);
+        image.setBlog(blog);
+
+        // this will handle the base condition;
+        //if your imagelist is null
+
+        List<Image>  listofimages= blog.getImageList();
+        if (Objects.isNull(listofimages)) {
+            listofimages = new ArrayList<>();
+        }
+        listofimages.add(image);
+        blog.setImageList(listofimages);
+        imageRepository2.save(image);
+        blogRepository.save(blog);
+        return image;
     }
 
     public void deleteImage(Image image){
+        imageRepository2.deleteImage(image);
 
     }
 
     public Image findById(int id) {
-
+        return imageRepository2.findById(id).get();
     }
 
-    public int countImagesInScreen(Image image, String screenDimensions) {
+    public int countImagesInScreen(int image, String screenDimensions) {
         //Find the number of images of given dimensions that can fit in a screen having `screenDimensions`
         //In case the image is null, return 0
+        return 0;
+    }
+
+    public void save(Image image) {
 
     }
 }
